45cfa846014b6a78d98a89efdfa0aa1342d12ae3
Brian O'Connor <briandoconnor@gmail.com>
2015-11-05 13:11:49 -0500

    updates for consonance

--- schedule_workflow.pl
+++ schedule_workflow.pl
@@ -268,7 +268,10 @@ sub write_status {
     if ($output =~ /^\{/) {
       my $job_info_hash = decode_json($output);
       my $status = $job_info_hash->{state};
-      print OUT "$job_proj\t$job_uuid\t$status\t$job_config\n";
+      my $line = "$job_proj\t$job_uuid\t$status\t$job_config\n";
+      if ($line =~ /\w/) {
+        print OUT $line;
+      }
     }
   }
   close OUT;
